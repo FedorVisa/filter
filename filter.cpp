fae45ba1ac677005190a266b1012ff8d5da03c40
FedorVisa <floodywizard@gmail.com>
2021-03-29 15:18:39 +0300

    norm vvod

--- filter.cpp
+++ filter.cpp
@@ -1,24 +1,24 @@
 ﻿#include <iostream>
 #include <iomanip>
 #define k 0.8
-double* filter(double* input, double* output) {
-	for (int i = 0; i < 10; i++) {
-		if (i == 0)output[i] = input[i];
-		else output[i] = output[i - 1] * k + input[i] * (1 - k);
-	}
+double filter(double input, double output) {
+	
+	 output = output * k + input * (1 - k);
+	
 	return output;
 }
 
 int main()
 {
 	double input[10];
-	double output[10];
+	
 	for (int i = 0; i < 10; i++) {
 		std::cin >> input[i];
 	}
-	filter(input, output);
+	double output = input[0];
 
-	for (int i = 0; i < 10; i++) {
-		std::cout << std::fixed << std::setprecision(6) << output[i]<<std::endl;
+	for (int i = 1; i < 10; i++) {
+		output=filter(input[i], output);
+		std::cout << std::fixed << std::setprecision(6) << output << std::endl;
 	}
 }
